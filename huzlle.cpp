e961912e30585f297fd01a7139cc7cad60d8988a
darenakpinar <darenakpinar@gmail.com>
2025-04-22 15:32:28 +0200

    m

---
+++ huzlle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+if (b.at(r) . at(c)!= w.at(pas) || sal.at(r) . at(c)>0) return false;
+else {
+	sol.st(r).at(c) =  pas + 1;
+	bool rep = false;
+	for (int k = max(0, r-1); k++){
+		for (int j = max (0, c-1); i < min (4, r+1); k++){
+			rep = rep||search(t,w, sal, k, j, pos+1);
+	if (rep)return true;
+	else {
+	    sal at(r) at(c) = 0;
+	    return false;
+	}
+// backtrcing method easy to roll back recursive search for a word in 
+
+
+// empty chess board we need to place 8 wuinc to 8 rows   use backewerds moethod recursion solutio.
