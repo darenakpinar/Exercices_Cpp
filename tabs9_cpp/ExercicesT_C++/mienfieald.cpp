2ae9ffbfb63a28125338f58f30912bfc3e8f4670
darenakpinar <darenakpinar@gmail.com>
2025-04-29 21:52:42 +0200

    I updatedd simplers oop 2.5

---
+++ tabs9_cpp/ExercicesT_C++/mienfieald.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class flowerfield{
+	public:
+		flowefield(int rows, int cols);
+		void create(int flowers);
+		bool uncover(list n, int c);
+		bool victory() const;
+		bool display() const;
+	private:
+		vector<vector<int>>;
+		int flowers;
+		vector<vector<bool>>state;
+	};
+	flowerField::flowe 
+
